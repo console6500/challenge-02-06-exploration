5ea8a2190314e337c40ba378379f2476503f5089
console6500 <73613222+console6500@users.noreply.github.com>
2023-03-18 16:53:08 -0700

    Update pandas_version_test.py

--- pandas_version_test.py
+++ pandas_version_test.py
@@ -1,6 +1,5 @@
-import unittest
 import pandas as pd
 
-class Test(unittest.TestCase):
-    def test_pandas_version():
-        assert pd.__version__ in ["1.5.3"]
+
+def test_pandas_version():
+    assert pd.__version__ in ["1.5.3"]
